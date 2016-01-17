398c1f4be16bc8d982bd83ea60523ec4452b0c7d
David Lintern <David.Lintern@myport.ac.uk>
2016-01-17 11:01:41 +0000

    Added a simple setup example

---
+++ examples/SimpleSetup/SimpleSetup.ino
@@ -0,0 +1,52 @@
+/* This example is a simple setup example to show you how to setup the AutoHome library. */
+/* If you run this example with the variables filled out the esp will print out any */
+/* received packets from the mqtt broker */
+
+#include <AutoHome.h>
+
+/* These variables need to be filled out with the information for your wifi details */
+/* and the mqtt broker */
+String wifi_ssid = "";
+String wifi_password = "";
+String mqtt_broker_ip = "";
+String mqtt_username = "";
+String mqtt_password = "";
+String host_name = "";
+String mqtt_topic = "";
+
+AutoHome autohome;
+
+/* This function will be called every time a packet is received from the mqtt topic. */
+/* This is registered in the setup() */
+void mqtt_callback(char* topic, byte* payload, unsigned int length){
+
+  String packet = "";
+
+  for (int i = 0; i < length; i++) {
+    packet = packet + (char)payload[i];
+  }
+
+  Serial.print(packet);
+  
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+
+  /* This registers the function that gets called when a packet is recieved. */
+  autohome.setPacketHandler(mqtt_callback);
+
+  /* This starts the library and connects the esp to the wifi and the mqtt broker */
+  autohome.begin(wifi_ssid, wifi_password, mqtt_broker_ip, mqtt_user, mqtt_password, host_name, mqtt_topic);
+
+}
+
+void loop() {
+
+  /* This needs to be called in the loop as it handels the reconection to the mqtt server if it disconnects*/
+  autohome.loop();
+
+}
